461e524a4ff0767253938cae22c961365092c756
Vikash <vikash211004@gmail.com>
2025-01-21 05:45:21 +0530

    stating setup & worked on Header, Navs, Framer Motion Transitions

---
+++ my-portfolio/app/page.jsx
@@ -0,0 +1,6 @@
+//
+const Home = () => {
+  return <div>homepage</div>;
+};
+
+export default Home;
---
+++ my-portfolio/app/services/page.jsx
@@ -0,0 +1,5 @@
+const Services = () => {
+    return <div>service page</div>
+};
+
+export default Services;
